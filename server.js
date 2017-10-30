d798044845cf30e5eefb9744400336809c158ba9
Rob Aird <emailrobaird@gmail.com>
2017-10-30 15:34:12 -0500

    Express server is up and running

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+// const router = express.Router();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.send('helloooo!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
