7c7728a0705411675f3b1372189074018d0bb613
Rob Aird <emailrobaird@gmail.com>
2017-10-30 15:39:14 -0500

    Serving blank index.html

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.send('helloooo!');
+  res.send('public/index.html');
 });
 
 app.listen(port, () => {
