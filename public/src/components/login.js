58a1b122e907e8a18f19c942683e96294a3e0428
Rob Aird <emailrobaird@gmail.com>
2017-10-31 12:03:34 -0500

    Beginning to implement Instagram redirect

---
+++ public/src/components/login.js
@@ -0,0 +1,6 @@
+angular.module('instaMapped')
+  .component('login', {
+    controller: function loginControl() {
+      this.instagramUrl = `https://api.instagram.com/oauth/authorize/?client_id=${window.INSTAGRAM_ID}&redirect_uri={${window.REDIRECT_URI}&response_type=code`;
+    },
+  });
