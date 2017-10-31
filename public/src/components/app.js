32ebae705f4e13c45f9e41eb366f5b2f0af43195
Rob Aird <emailrobaird@gmail.com>
2017-10-31 12:14:58 -0500

    Successfully directing to Instagram Authorization Screen!

--- public/src/components/app.js
+++ public/src/components/app.js
@@ -2,7 +2,7 @@ angular.module('instaMapped')
   .component('app', {
     controller: function appControl() {
       this.googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${window.GOOGLE_MAP_KEY}`;
-      console.log(this.googleMapsUrl);
+      this.instagramUrl = `https://api.instagram.com/oauth/authorize/?client_id=${window.INSTAGRAM_ID}&redirect_uri=${window.REDIRECT_URI}&response_type=code`;
     },
-    templateUrl: '/src/templates/app.html',
+    templateUrl: '/src/templates/login.html',
   });
--- public/src/components/login.js
+++ public/src/components/login.js
@@ -3,4 +3,5 @@ angular.module('instaMapped')
     controller: function loginControl() {
       this.instagramUrl = `https://api.instagram.com/oauth/authorize/?client_id=${window.INSTAGRAM_ID}&redirect_uri={${window.REDIRECT_URI}&response_type=code`;
     },
+    templateUrl: '/src/templates/login.html',
   });
