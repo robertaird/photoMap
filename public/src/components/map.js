e6b3c78c85062d8cc3317d7137f8a531c33948e2
Rob Aird <emailrobaird@gmail.com>
2017-11-01 00:57:51 -0500

    Test image is displaying on map -- going to bed.

--- public/src/components/map.js
+++ public/src/components/map.js
@@ -5,9 +5,17 @@ angular.module('instaMapped')
     },
     controller: function mapControl($http) {
       this.photos = [];
+      this.testUrl = 'https://scontent.cdninstagram.com/t51.2885-15/s150x150/e35/c107.0.866.866/21689161_493570764361728_6028430128323756032_n.jpg';
+      this.testIcon = {
+      url:'https://scontent.cdninstagram.com/t51.2885-15/s150x150/e35/c107.0.866.866/21689161_493570764361728_6028430128323756032_n.jpg',
+      scaledSize:[50,50],
+      origin: [0,0],
+      anchor: [25,25] 
+      }
       this.getPhotos = (user) => {
         $http.get(`http://localhost:8000/photos?id=${user}`).then((result) => {
           this.photos = result.data;
+          console.log(this.photos);
         });
       };
       this.$onInit = () => {
