4f71c6aa4a4e161f3124a518ccd642e999dae6c9
Rob Aird <emailrobaird@gmail.com>
2017-10-31 19:22:18 -0500

    Fixed routing for log in to maps

--- public/src/components/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('instaMapped')
-  .component('app', {
-    controller: function appControl() {
-      this.googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${window.GOOGLE_MAP_KEY}`;
-      this.instagramUrl = `https://api.instagram.com/oauth/authorize/?client_id=${window.INSTAGRAM_ID}&redirect_uri=${window.REDIRECT_URI}&response_type=code`;
-    },
-    templateUrl: '/src/templates/login.html',
-  });
