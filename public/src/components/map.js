8e37605edcea791a32d2e8790e3e43b98c44c544
Rob Aird <emailrobaird@gmail.com>
2017-11-01 17:09:53 -0500

    Basic modal functionality

--- public/src/components/map.js
+++ public/src/components/map.js
@@ -3,7 +3,7 @@ angular.module('instaMapped')
     bindings: {
       user: '<',
     },
-    controller: function mapControl($http) {
+    controller: function mapControl($http, $uibModal) {
       this.googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${window.GOOGLE_MAP_KEY}`;
       this.photos = [{ location: { latitude: 43, longitude: 34 } },
         { location: { latitude: 3, longitude: 37 } },
@@ -11,9 +11,12 @@ angular.module('instaMapped')
 
       this.showInfo = (event, id) => {
         console.log(event, id);
-        // $http.post(`http://localhost:8000/photos?id=${id}`).then((result) => {
-        //   console.log(result);
-        // });
+        $uibModal.open({
+          templateUrl: '/src/templates/modal.html',
+        });
+        $http.post(`http://localhost:8000/photos?id=${id}`).then((result) => {
+          console.log(result);
+        });
       };
 
       this.generateThumbnails = (photos) => {
