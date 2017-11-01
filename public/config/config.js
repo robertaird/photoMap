89814d4788525569b05a56a12b0562a181d1158b
Rob Aird <emailrobaird@gmail.com>
2017-11-01 17:57:01 -0500

    bout to deploy

---
+++ public/config/config.js
@@ -0,0 +1,3 @@
+window.GOOGLE_MAP_KEY = 'AIzaSyAB4mbNTZdkIEdaflCFstGw3jxeA5R-nGw';
+window.INSTAGRAM_ID = '00ad1fe243bd4aceae5a770664039a24';
+window.REDIRECT_URI = 'http://photomapped.herokuapp.com/map/';
